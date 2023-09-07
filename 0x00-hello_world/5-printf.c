ef47fd0a9e9648aabd53e306264ad35fe5b20436
Yassin9323 <Yassin.waleed94@gmail.com>
2023-09-07 07:16:10 -0700

    GOT

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+/**
+ * main display: with proper grammar, but the outcome is a piece of art,
+ * return 0 and exit
+ */
+
+int main (void)
+	{
+		printf("with proper grammar, but the outcome is a piece of art,");
+		return(0);
+	
+	
+	
+	
+	
+	}
