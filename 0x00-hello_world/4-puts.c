e1c720d915be6f2f90265a809e29caef343b45c8
Yassin9323 <Yassin.waleed94@gmail.com>
2023-09-07 07:07:21 -0700

    GOT

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ * main - display  "Programming is like building a multilingual puzzle"
+ * Return: 0 and exit the program
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
