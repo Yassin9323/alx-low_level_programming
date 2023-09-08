298a53185e6c5896d806e0a286329ed95665110d
Yassin9323 <Yassin.waleed94@gmail.com>
2023-09-08 16:35:45 -0700

    TWD

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - display alphabet in reverse and lowercase
+ * main - display alphabet reverse and lowercase
  * Return: return 0 and exit the program
  */
 int main(void)
 {
-    char l;
+	char l;
 
-    for (l = 'z'; l >= 'a'; l--)
-    {
-        putchar(l);
-    }
-    putchar('\n');
+	for (l = 'z'; l >= 'a'; l--)
+      putchar(l);
+      putchar('\n');
 
-    return (0);
+	return (0);
 }
+
