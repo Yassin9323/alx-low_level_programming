23f1d4d4122ebf87e4a8875d587143482f199c34
Yassin9323 <Yassin.waleed94@gmail.com>
2023-09-12 11:57:20 -0700

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,33 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ *  * print_putchar - Printing a _putchar string
+ *   *
+ *    * Description: Printing a _putchar string
+ *     * @string: A string of characters
+ *      */
+void print_putchar(char string[])
+{
+		int i = 0;
+
+			while (string[i] != '\0')
+					{
+							putchar(string[i]);
+								i++;
+									}
+}
 /**
  *  * main - Entry point
  *   *
- *    * Description: Prints _putchar, followed by a new line
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-		char c[] = "_putchar\n";
-			int i;
+		char string[] = "_putchar";
 
-				for (i = 0; message[i] != '\0'; i++)
-						{
-									/* Use the _putchar function to print each character */
-									_putchar(c[i]);
-										}
+			print_putchar(string);
+				putchar('\n');
 
 					return (0);
 }
